311cfc3adf22a84c1c32562017ae6b4e6e79037c
aug <yfpan16@fudan.edu.cn>
2020-11-25 17:40:26 +0800

    debug & added data from group B

--- pm25.R
+++ pm25.R
@@ -3,23 +3,23 @@ library(tidyverse)
 # PART1 读取全部pm2.5个体暴露数据
 ## 这些数据储存在data/PM2.5文件夹下，文件名必须是实验对象的编号如A1.txt等
 ## 扫描data/PM2.5文件
-flist = list.files("./pm25")
+flist = list.files("data/PM2.5")
 sp = strsplit(flist, "\\.")
 
 ## 重新定义英文列名
-cname = c("index", "date", "time", "pm1_std", "pm25_std", "pm10_std", "pm1_gas", "pm25_gas", "pm10_gas", "voltage", "empty")
+cname = c("index", "date", "time", "pm1", "pm25", "pm10", "pm1_skip", "pm25_skip", "pm10_skip", "voltage", "empty")
 ## 定义变量类型
 ctype = cols(
   index = col_double(),
   date = col_character(),
   time = col_character(),
-  pm1_std = col_double(),
-  pm25_std = col_double(),
-  pm10_std = col_double(),
-  pm1_gas = col_double(),
-  pm25_gas = col_double(),
-  pm10_gas = col_double(),
-  voltage = col_double(),
+  pm1 = col_double(),
+  pm25 = col_double(),
+  pm10 = col_double(),
+  pm1_skip = col_skip(),
+  pm25_skip = col_skip(),
+  pm10_skip = col_skip(),
+  voltage = col_skip(),
   empty = col_skip()
 )
 
@@ -27,7 +27,7 @@ ctype = cols(
 df = tibble()
 for (i in 1:length(sp)) {
   if (sp[[i]][2] == "txt") {
-    tdf = read_tsv(paste0("./pm25/", sp[[i]][1], ".txt"), col_names = cname, col_types = ctype, skip=1)
+    tdf = read_tsv(paste0("data/PM2.5/", sp[[i]][1], ".txt"), col_names = cname, col_types = ctype, skip=1)
     tdf$object = sp[[i]][1]
     
     date_temp = as.Date(tdf$date, "%y-%m-%d")
@@ -47,19 +47,18 @@ for (i in 1:length(sp)) {
 ## lag是需要平均的小时数
 calc_average = function(df, doi, lag) {
   df %>% 
-    mutate(time_diff=df$datetime-doi) %>%
+    mutate(time_diff=difftime(df$datetime, doi, units="mins")) %>%
     filter(time_diff >= -lag*60 & time_diff <= 0) %>%
     group_by(object) %>%
-    summarise(pm1_std = mean(pm1_std))
+    summarise(pm25 = mean(pm25), n = n())
 }
 
 # doi: date of interest
 ## doi1 一阶段结束
-doi2 = strptime("20-11-8 08-00", "%y-%m-%d %H-%M")
+doi1 = strptime("20-11-8 08-00", "%y-%m-%d %H-%M")
 doi2 = strptime("20-11-16 08-00", "%y-%m-%d %H-%M")
 
-doi = strptime("20-11-10 08-00", "%y-%m-%d %H-%M")
 # lag time (hour)
 lag = 8
 
-calc_average(df, doi, lag)
+calc_average(df, doi1, lag)
